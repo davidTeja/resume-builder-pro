d5aad6f526772c2b90c16c428d89ab649b7a7a9e
David Teja <pteja721hotmail@gmail.com>
2026-06-13 01:42:37 +0530

    Add more typography and components customisations

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({
+const baseTheme = createTheme({
     palette: {
         primary: {
             light: "#10B981",
@@ -15,8 +15,8 @@ const theme = createTheme({
             contrastText: "#FFFFFF",
         },
         background: {
-              default: "#F8F9FA",
-              paper: "#FFFFFF",
+            default: "#F8F9FA",
+            paper: "#FFFFFF",
         },
         text: {
             primary: "#212529",
@@ -42,22 +42,100 @@ const theme = createTheme({
     },
     typography: {
         fontFamily: "'Inter', sans-serif",
-        h1: {
-            fontWeight: 800,
-        },
-        h2: {
-            fontWeight: 700,
-        },
-        h3: {
-            fontWeight: 600,
-        },
-        // button: {
-        //      fontWeight: 600,
-        //      textTransform: "none",
-        // },
     },
     shape: {
-          borderRadius: 16,
+        borderRadius: 16,
+    },
+});
+
+const theme = createTheme(baseTheme, {
+    typography: {
+        h1: {
+            fontWeight: 800,
+            lineHeight: 1.25,
+            fontSize: "4rem",
+            [baseTheme.breakpoints.down("lg")]: { fontSize: "3.5rem" },
+            [baseTheme.breakpoints.down("md")]: { fontSize: "3rem" },
+            [baseTheme.breakpoints.down("sm")]: { fontSize: "2.5rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "2.25rem" },
+        },
+        h2: {
+            fontWeight: 800,
+            lineHeight: 1.2,
+            fontSize: "3.75rem",
+            [baseTheme.breakpoints.down("lg")]: { fontSize: "3.5rem" },
+            [baseTheme.breakpoints.down("md")]: { fontSize: "3rem" },
+            [baseTheme.breakpoints.down("sm")]: { fontSize: "2.5rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "2rem" },
+        },
+        h3: {
+            fontWeight: 700,
+            lineHeight: 1.2,
+            fontSize: "2.25rem",
+            [baseTheme.breakpoints.down("md")]: { fontSize: "2rem" },
+            [baseTheme.breakpoints.down("sm")]: { fontSize: "1.75rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "1.5rem" },
+        },
+        h4: {
+            fontWeight: 700,
+            lineHeight: 1.25,
+            fontSize: "1.75rem",
+            [baseTheme.breakpoints.down("md")]: { fontSize: "1.5rem" },
+            [baseTheme.breakpoints.down("sm")]: { fontSize: "1.35rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "1.25rem" },
+        },
+        h5: {
+            fontWeight: 600,
+            lineHeight: 1.3,
+            fontSize: "1.35rem",
+            [baseTheme.breakpoints.down("md")]: { fontSize: "1.25rem" },
+            [baseTheme.breakpoints.down("sm")]: { fontSize: "1.15rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "1.1rem" },
+        },
+        h6: {
+            fontWeight: 600,
+            lineHeight: 1.3,
+            fontSize: "1.25rem",
+            [baseTheme.breakpoints.down("md")]: { fontSize: "1.1rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "1rem" },
+        },
+        subtitle1: {
+            fontWeight: 400,
+            lineHeight: 1.5,
+            fontSize: "1.25rem",
+            [baseTheme.breakpoints.down("md")]: { fontSize: "1.15rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "1.05rem" },
+        },
+        subtitle2: {
+            fontWeight: 500,
+            lineHeight: 1.5,
+            fontSize: "1.05rem",
+            [baseTheme.breakpoints.down("md")]: { fontSize: "1rem" },
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "0.95rem" },
+        },
+        body1: {
+            fontWeight: 400,
+            lineHeight: 1.5,
+            fontSize: "1rem",
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "0.95rem" },
+        },
+        body2: {
+            fontWeight: 400,
+            lineHeight: 1.5,
+            fontSize: "0.875rem",
+            [baseTheme.breakpoints.down("xs")]: { fontSize: "0.825rem" },
+        },
+        button: {
+            fontWeight: 600,
+            lineHeight: 1.7,
+            fontSize: "0.920rem",
+            textTransform: "none",
+        },
+        caption: {
+            fontWeight: 400,
+            lineHeight: 1.4,
+            fontSize: "0.75rem",
+        },
     },
 });
 
